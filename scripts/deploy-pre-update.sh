18dc0a18aa607bf9f373bfef13e0cb6bc186f140
Llorenç Vaquer <llorenc.vaquer@ithinkupc.com>
2026-06-01 06:29:15 +0200

    Fix problema del ci local

--- scripts/deploy-pre-update.sh
+++ scripts/deploy-pre-update.sh
@@ -15,7 +15,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 
 # Variables (pots canviar-les segons necessitat)
-BRANCH="${1:-merge/pinyes-cordons-i-rengles}"
+BRANCH="${1:-story/deploy-server-pre}"
 
 echo -e "${YELLOW}📦 Actualitzant codi des de la branca: ${BRANCH}${NC}"
 git fetch origin
