5fa56e01e56a062844eabbe96a0a03d02f4a57fc
Ferran López <flg@tuta.io>
2026-06-27 16:03:24 +0200

    fix: visualitzacio vista tablet

--- apps/dashboard/src/app/features/events/components/attendance-confirmation/attendance-confirmation.component.ts
+++ apps/dashboard/src/app/features/events/components/attendance-confirmation/attendance-confirmation.component.ts
@@ -15,7 +15,7 @@ import { AttendanceStatus } from '@muixer/shared';
 const KEYBOARD_ROWS = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ç'],
-  ['Z', 'X', 'C', 'V', 'B', 'N', 'M', '⌫'],
+  ['', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '⌫', ''],
 ];
 
 @Component({
